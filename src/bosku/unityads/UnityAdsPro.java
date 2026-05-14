d96fe63f2a2e20790dfcb9cc253d9d089956ec95
bansos567 <notopribadi25@gmail.com>
2026-05-15 00:33:04 +0700

    Update UnityAdsPro.java

--- src/bosku/unityads/UnityAdsPro.java
+++ src/bosku/unityads/UnityAdsPro.java
@@ -15,7 +15,7 @@ import com.unity3d.ads.UnityAdsShowOptions;
 import com.unity3d.services.banners.BannerView;
 import com.unity3d.services.banners.UnityBannerSize;
 
-@DesignerComponent(version = 1, description = "Unity Ads Full Extension by Bosku", category = ComponentCategory.EXTENSION, nonVisible = true, iconName = "images/extension.png")
+@DesignerComponent(version = 2, description = "Unity Ads Full Extension by Bosku", category = ComponentCategory.EXTENSION, nonVisible = true, iconName = "images/extension.png")
 @SimpleObject(external = true)
 @UsesLibraries(libraries = "unity-ads.aar")
 @UsesPermissions(permissionNames = "android.permission.INTERNET, android.permission.ACCESS_NETWORK_STATE")
@@ -38,12 +38,22 @@ public class UnityAdsPro extends AndroidNonvisibleComponent {
         UnityAds.initialize(context, gameId, testMode, new IUnityAdsInitializationListener() {
             @Override
             public void onInitializationComplete() {
-                InitializationSuccess();
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        InitializationSuccess();
+                    }
+                });
             }
 
             @Override
-            public void onInitializationFailed(UnityAds.UnityAdsInitializationError error, String message) {
-                InitializationFailed(message);
+            public void onInitializationFailed(final UnityAds.UnityAdsInitializationError error, final String message) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        InitializationFailed(message);
+                    }
+                });
             }
         });
     }
@@ -51,17 +61,27 @@ public class UnityAdsPro extends AndroidNonvisibleComponent {
     // ==========================================
     // FUNGSI LOAD & SHOW IKLAN
     // ==========================================
-    @SimpleFunction(description = "Load Iklan berdasarkan Ad Unit ID (misal: Rewarded_Android)")
+    @SimpleFunction(description = "Load Iklan berdasarkan Ad Unit ID")
     public void LoadAd(final String adUnitId) {
         UnityAds.load(adUnitId, new IUnityAdsLoadListener() {
             @Override
-            public void onUnityAdsAdLoaded(String placementId) {
-                AdLoaded(placementId);
+            public void onUnityAdsAdLoaded(final String placementId) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        AdLoaded(placementId);
+                    }
+                });
             }
 
             @Override
-            public void onUnityAdsFailedToLoad(String placementId, UnityAds.UnityAdsLoadError error, String message) {
-                AdFailedToLoad(placementId, message);
+            public void onUnityAdsFailedToLoad(final String placementId, UnityAds.UnityAdsLoadError error, final String message) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        AdFailedToLoad(placementId, message);
+                    }
+                });
             }
         });
     }
@@ -70,24 +90,44 @@ public class UnityAdsPro extends AndroidNonvisibleComponent {
     public void ShowAd(final String adUnitId) {
         UnityAds.show(activity, adUnitId, new UnityAdsShowOptions(), new IUnityAdsShowListener() {
             @Override
-            public void onUnityAdsShowFailure(String placementId, UnityAds.UnityAdsShowError error, String message) {
-                AdFailedToShow(placementId, message);
+            public void onUnityAdsShowFailure(final String placementId, UnityAds.UnityAdsShowError error, final String message) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        AdFailedToShow(placementId, message);
+                    }
+                });
             }
 
             @Override
-            public void onUnityAdsShowStart(String placementId) {
-                AdStarted(placementId);
+            public void onUnityAdsShowStart(final String placementId) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        AdStarted(placementId);
+                    }
+                });
             }
 
             @Override
-            public void onUnityAdsShowClick(String placementId) {
-                AdClicked(placementId);
+            public void onUnityAdsShowClick(final String placementId) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        AdClicked(placementId);
+                    }
+                });
             }
 
             @Override
-            public void onUnityAdsShowComplete(String placementId, UnityAds.UnityAdsShowCompletionState state) {
-                boolean isCompleted = state.equals(UnityAds.UnityAdsShowCompletionState.COMPLETED);
-                AdCompleted(placementId, isCompleted);
+            public void onUnityAdsShowComplete(final String placementId, final UnityAds.UnityAdsShowCompletionState state) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        boolean isCompleted = state.equals(UnityAds.UnityAdsShowCompletionState.COMPLETED);
+                        AdCompleted(placementId, isCompleted);
+                    }
+                });
             }
         });
     }
@@ -95,7 +135,7 @@ public class UnityAdsPro extends AndroidNonvisibleComponent {
     // ==========================================
     // FUNGSI BANNER IKLAN
     // ==========================================
-    @SimpleFunction(description = "Tampilkan Iklan Banner di dalam Horizontal/Vertical Arrangement Kodular")
+    @SimpleFunction(description = "Tampilkan Iklan Banner di dalam Horizontal/Vertical Arrangement")
     public void ShowBanner(AndroidViewComponent layoutContainer, String adUnitId) {
         try {
             ViewGroup viewGroup = (ViewGroup) layoutContainer.getView();
@@ -108,86 +148,46 @@ public class UnityAdsPro extends AndroidNonvisibleComponent {
         }
     }
 
-        // ==========================================
-    // EVENTS (Blocks Kuning)
+    // ==========================================
+    // EVENTS (Blocks Kuning) - SEKARANG MURNI
     // ==========================================
     @SimpleEvent(description = "Terpanggil saat inisialisasi sukses")
     public void InitializationSuccess() {
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                EventDispatcher.dispatchEvent(UnityAdsPro.this, "InitializationSuccess");
-            }
-        });
+        EventDispatcher.dispatchEvent(this, "InitializationSuccess");
     }
 
     @SimpleEvent(description = "Terpanggil saat inisialisasi gagal")
-    public void InitializationFailed(final String errorMessage) {
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                EventDispatcher.dispatchEvent(UnityAdsPro.this, "InitializationFailed", errorMessage);
-            }
-        });
+    public void InitializationFailed(String errorMessage) {
+        EventDispatcher.dispatchEvent(this, "InitializationFailed", errorMessage);
     }
 
     @SimpleEvent(description = "Terpanggil saat iklan siap ditampilkan")
-    public void AdLoaded(final String adUnitId) {
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                EventDispatcher.dispatchEvent(UnityAdsPro.this, "AdLoaded", adUnitId);
-            }
-        });
+    public void AdLoaded(String adUnitId) {
+        EventDispatcher.dispatchEvent(this, "AdLoaded", adUnitId);
     }
 
     @SimpleEvent(description = "Terpanggil saat iklan gagal di-load")
-    public void AdFailedToLoad(final String adUnitId, final String errorMessage) {
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                EventDispatcher.dispatchEvent(UnityAdsPro.this, "AdFailedToLoad", adUnitId, errorMessage);
-            }
-        });
+    public void AdFailedToLoad(String adUnitId, String errorMessage) {
+        EventDispatcher.dispatchEvent(this, "AdFailedToLoad", adUnitId, errorMessage);
     }
 
     @SimpleEvent(description = "Terpanggil saat iklan gagal ditampilkan")
-    public void AdFailedToShow(final String adUnitId, final String errorMessage) {
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                EventDispatcher.dispatchEvent(UnityAdsPro.this, "AdFailedToShow", adUnitId, errorMessage);
-            }
-        });
+    public void AdFailedToShow(String adUnitId, String errorMessage) {
+        EventDispatcher.dispatchEvent(this, "AdFailedToShow", adUnitId, errorMessage);
     }
 
     @SimpleEvent(description = "Terpanggil saat iklan mulai muncul di layar")
-    public void AdStarted(final String adUnitId) {
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                EventDispatcher.dispatchEvent(UnityAdsPro.this, "AdStarted", adUnitId);
-            }
-        });
+    public void AdStarted(String adUnitId) {
+        EventDispatcher.dispatchEvent(this, "AdStarted", adUnitId);
     }
 
     @SimpleEvent(description = "Terpanggil saat user mengklik iklan")
-    public void AdClicked(final String adUnitId) {
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                EventDispatcher.dispatchEvent(UnityAdsPro.this, "AdClicked", adUnitId);
-            }
-        });
+    public void AdClicked(String adUnitId) {
+        EventDispatcher.dispatchEvent(this, "AdClicked", adUnitId);
     }
 
     @SimpleEvent(description = "Terpanggil saat iklan selesai")
-    public void AdCompleted(final String adUnitId, final boolean isCompleted) {
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                EventDispatcher.dispatchEvent(UnityAdsPro.this, "AdCompleted", adUnitId, isCompleted);
-            }
-        });
+    public void AdCompleted(String adUnitId, boolean isCompleted) {
+        EventDispatcher.dispatchEvent(this, "AdCompleted", adUnitId, isCompleted);
     }
 }
